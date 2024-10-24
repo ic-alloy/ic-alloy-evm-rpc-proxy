9d600e699349c3b909b83973ed610403c00f05f4
Kristofer <kristofer@fmckl.se>
2024-10-24 16:08:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# A deduplicating EVM RPC proxy for Cloudflare
+
+Runs a Cloudflare Worker that proxies and deduplicates EVM RPC requests to the [Alchemy](https://www.alchemy.com) API. The worker is deployed to Cloudflare's [edge infrastructure](https://www.cloudflare.com/network/).
+
+The intended use case is to reduce the number of requests to the Alchemy API by deduplicating identical requests. This is useful when multiple clients are querying the same data, such as when making requests through the EVM RPC Canister on the Internet Computer.
+
+The worker will cache the response from Alchemy and return the cached response to all subsequent requests with the same parameters.
+
+## Install on Cloudflare
+
+### 1. Pre-requisites
+
+Set up a Cloudflare account and install the Wrangler CLI. Wrangler is used to develop, deploy, and configure your Worker via CLI.
+
+Further documentation for Wrangler can be found [here](https://developers.cloudflare.com/workers/tooling/wrangler).
+
+The worker is configured using the following environment variables:
+
+- `ALCHEMY_API_KEY`: The API key to use when making requests to Alchemy.
+
+### 2. Configure
+
+Configure the worker by setting the environment variable using:
+
+```bash
+npx wrangler secret put ALCHEMY_API_KEY
+```
+
+### 3. Deploy
+
+Deploy the worker to Cloudflare using:
+
+```bash
+pnpm i
+pnpm run deploy
+```
+
+### 4. Usage
+
+In your IC project, to configure the EVM RPC Canister to use the proxy instead of the default services, see the following example:
+
+```rust
+let base_url = "https://<proxy-url-on-cludflare>.workers.dev";
+
+// For ETH Sepolia
+let rpc_services = RpcServices::Custom {
+  chainId: 11155111,
+  services: vec![RpcApi {
+    url: format!("{}/{}", base_url, "eth-sepolia"),
+    headers: None,
+  }],
+},
+```
+
+## Run locally
+
+### 1. Configure
+
+- Create a `.dev.vars` file in the root of the project.
+- Add the following content to the file:
+
+```bash
+ALCHEMY_API_KEY=<API_KEY>
+```
+
+### 2. Run
+
+```bash
+pnpm i
+pnpm run dev
+```
+
+## Collaborators
+
+<!-- readme: collaborators,contributors -start -->
+<table>
+ <tbody>
+  <tr>
+            <td align="center">
+                <a href="https://github.com/kristoferlund">
+                    <img src="https://avatars.githubusercontent.com/u/9698363?v=4" width="100;" alt="kristoferlund"/>
+                    <br />
+                    <sub><b>Kristofer</b></sub>
+                </a>
+            </td>
+  </tr>
+ <tbody>
+</table>
+<!-- readme: collaborators,contributors -end -->
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for more details.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request if you have any suggestions or improvements.
---
+++ src/index.ts
@@ -0,0 +1,107 @@
+import { DurableObject } from "cloudflare:workers";
+import { blake3 } from '@noble/hashes/blake3';
+
+export interface Env {
+  RPC_PROXY: DurableObjectNamespace<RpcProxy>;
+  ALCHEMY_API_KEY: string;
+}
+
+const enum RpcProxyState {
+  NotStarted,
+  InProgress,
+  Done,
+}
+
+function toHexString(byteArray: Uint8Array) {
+  return Array.from(byteArray, byte => {
+    return byte.toString(16).padStart(2, '0');
+  }).join('');
+}
+
+// RpcProxy is a Durable Object that proxies JSON-RPC requests to Alchemy.
+// Read more about Durable Objects here: https://developers.cloudflare.com/durable-objects/
+export class RpcProxy extends DurableObject {
+  alchemyApiKey: string;
+  state: RpcProxyState = RpcProxyState.NotStarted;
+  responseText?: string;
+  responseStatus?: number;
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+    this.alchemyApiKey = env.ALCHEMY_API_KEY;
+  }
+
+  async proxyRpcRequest(
+    rpcRequest: string,
+    chain: string
+  ): Promise<Response> {
+    if (this.state === RpcProxyState.NotStarted) {
+      this.state = RpcProxyState.InProgress;
+      const url = `https://${chain}.g.alchemy.com/v2/${this.alchemyApiKey}`;
+      try {
+        const response = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: rpcRequest,
+        });
+        this.responseText = await response.text();
+        this.responseStatus = response.status;
+      } catch (e) {
+        console.error(e);
+        this.responseText = "Couldn't fetch data";
+        this.responseStatus = 500;
+      }
+      this.state = RpcProxyState.Done;
+    }
+
+    if (this.state === RpcProxyState.InProgress) {
+      while (this.state === RpcProxyState.InProgress) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+
+    return new Response(this.responseText, { status: this.responseStatus });
+  }
+}
+
+// The fetch event handler for the worker initiates the RPC request to the Durable Object.
+// The Durable Object is fetched by its name, which is derived from the request ID.
+// One instance of the Durable Object is created for each request ID.
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    switch (request.method) {
+      case "OPTIONS":
+        return new Response(null, {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          },
+        });
+      case "GET":
+        return new Response("Method not allowed", { status: 405 });
+      case "POST":
+        const path = new URL(request.url).pathname;
+        const chain = path.slice(1);
+        if (!chain || typeof chain !== "string") {
+          return new Response(
+            "Invalid chain. Call this endpoint with a url in this format: https:://<worker-url>/<chain>",
+            { status: 400 }
+          );
+        }
+        const rpcRequest = await request.text();
+        const requestId = blake3(rpcRequest);
+        let id: DurableObjectId = env.RPC_PROXY.idFromName(
+          toHexString(requestId)
+        );
+        let stub = env.RPC_PROXY.get(id);
+        return stub.proxyRpcRequest(rpcRequest, chain);
+    }
+    return new Response("Bad request", { status: 400 });
+  },
+};
